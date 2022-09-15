cd46bc948ed2dd6e4f76d1cda25fd6651d2150d6
ninevdsande <74553220+Ninevdsande@users.noreply.github.com>
2022-09-15 10:50:28 +0000

    kleiner dingen veranderd

--- main.py
+++ main.py
@@ -1,15 +1,20 @@
-from bke import EvaluationAgent, start, can_win
+from bke import EvaluationAgent, is_winner, opponent, start
 
 class MijnSpeler(EvaluationAgent):
   def evaluate(self, board, my_symbol, opponent_symbol):
     getal = 1
-    if can_win(board, opponent_symbol):
-        getal = getal -1000
-    return getal
+    
     if board[4] == my_symbol:
       getal = getal + 5
     return getal
-
+    if is_winner(board, my_symbol):
+            reward = 1
+    elif is_winner(board, opponent[my_symbol]):
+            reward = -1
+    else:
+            reward = 0
+    return reward()
+    
 
 mijn_speler = MijnSpeler()
 start(player_o=mijn_speler)
