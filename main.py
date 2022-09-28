953a9aa9f4b6810336aaaa99e275ddd4ed159f8a
ninevdsande <74553220+Ninevdsande@users.noreply.github.com>
2022-09-28 08:05:48 +0000

    Begonnen met het menu maken, 5 van het menu in al ingevoerd

--- main.py
+++ main.py
@@ -12,16 +12,33 @@ class MyAgent(MLAgent):
         else:
             reward = 0
         return reward
-    
-    
-random.seed(1)
- 
-my_agent = MyAgent(alpha=0.8, epsilon=0.2)
-random_agent = RandomAgent()
- 
-train_and_plot(
-    agent=my_agent,
-    validation_agent=random_agent,
-    iterations=50,
-    trainings=100,
-    validations=1000)
+
+def train():
+  random.seed(1)
+   
+  my_agent = MyAgent(alpha=0.8, epsilon=0.2)
+  random_agent = RandomAgent()
+   
+  train_and_plot(
+      agent=my_agent,
+      validation_agent=random_agent,
+      iterations=50,
+      trainings=100,
+      validations=1000)
+  
+print("1: Tegen een ander persoon spelen ")
+print("2: Tegen een domme tegenstander spelen")
+print("3: De tegenstander trainen")
+print("4: Tegen een slimme tegenstander spelen")
+print("5: Je programma kan de validatie grafiek plotten")
+
+i = input()
+
+if i == "5":
+  train()
+
+if i == "b":
+  # doe dat andere
+  aap = ""
+
+
