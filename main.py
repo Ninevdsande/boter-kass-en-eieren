41c2dc2dd19d0f7e8fd4a40e5072dc78326a2881
ninevdsande <74553220+Ninevdsande@users.noreply.github.com>
2022-10-03 14:31:52 +0000

    uitleg hyperparameters

--- main.py
+++ main.py
@@ -21,6 +21,7 @@ def plot():
   random.seed(1)
    
   my_agent = MyAgent(alpha=0.8, epsilon=0.2)
+  # aplha en epsilon zijn hyperparameters. Dit zijn aanpasbare parameters waarmee je het modeltrainingsproces kunt beheren. De parameter wordt ingesteld voordat het leerproces begint. Deze parameters zijn afstembaar en kunnen direct van invloed zijn op hoe goed een model traint. Aplha is de leerfactor van de agent, dit bepaalt hoe snel de agent nieuwe kennis op neemt. Hoe hoger dit getal hoe sneller de agent oude kennis door nieuwe kennis zal vervangen. Epsilon is de verkenningsfactor van de agent. Dit bepaalt hoevaak de agent nieuwe dingen probeert. Hoe hoger dit getal hoe vaker de agent een willekeurige actie gebruikt inplaats van de beste actie die hij kent,
   random_agent = RandomAgent()
    
   train_and_plot(
